8dd6acff34ed970e0a0970bc9b189a68def1bb22
Ryan Seit <ryan@ryanseit.com>
2020-02-04 15:28:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,29 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def get_move
+  puts "Please enter 1-9:"
+  input = gets.strip.to_i
+  input.to_i-1
+end
+
+def position_taken?(board, index)
+  !(board[index] == " ")
+end
+
+def valid_move?(board, index)
+  # index is a number, is between 0 and 8 and the position is not taken
+  index.is_a?(Integer) && 
+  index.between?(0,8) && 
+  !position_taken?(board,index)
+end
